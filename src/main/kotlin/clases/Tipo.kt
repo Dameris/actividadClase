c783339459b418b3d8ea868edfc9063638474c14
Dameris <116887141+Dameris@users.noreply.github.com>
2023-02-16 17:18:16 +0100

    AA0002

--- src/main/kotlin/clases/Tipo.kt
+++ src/main/kotlin/clases/Tipo.kt
@@ -1,5 +1,13 @@
 package clases
 
 class Tipo(tipoPokemon: String) {
-    var tipoPokemon = tipoPokemon
+    private var tipoPokemon = tipoPokemon
+        set(value) {
+            val listaTipos = listOf("acero", "agua", "bicho", "dragon", "electrico", "fantasma", "fuego", "hada", "hielo", "lucha",
+                "normal", "planta", "psiquico", "roca", "siniestro", "tierra", "veneno", "volador")
+            field = if (value !in listaTipos) { throw IllegalArgumentException("El tipo no es válido") }
+            else { value }
+        }
+
+    init { this.tipoPokemon = tipoPokemon }
 }
